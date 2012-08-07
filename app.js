5db44f6a81e928dec7aabb54c5c3ef0c10922872
Amio <amio.cn@gmail.com>
2012-08-07 15:42:46 +0800

    Init.

---
+++ app.js
@@ -0,0 +1,136 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+	routes = require('./routes'),
+	http = require('http'),
+	path = require('path');
+
+var app = express();
+
+app.configure(function () {
+	app.set('port', process.env.PORT || 3000);
+//	app.set('views', __dirname + '/views');
+//	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function () {
+	app.use(express.errorHandler());
+});
+
+/**
+ * Main request handler
+ * @type {Object}
+ */
+var otlistReg = {}, allowReg = true, onlineCount = 0;
+app.get('/otdinner/:op/:name?', function (req, res) {
+	var op = req.params.op,
+		name = req.params.name;
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	switch (op) {
+		case 'get':
+			res.write(JSON.stringify({
+				status: 'success',
+				op: 'get',
+				table: otlistReg
+			}));
+			break;
+		case 'add':
+			if (otlistReg[name]) {
+				res.write(JSON.stringify({
+					status: 'Error:001',
+					msg: 'User already on the list.',
+					op: 'add',
+					person: name,
+					table: otlistReg
+				}));
+			}else{
+				otlistReg[name] = true;
+				res.write(JSON.stringify({
+					status: 'success',
+					op: 'add',
+					person: name,
+					table: otlistReg
+				}));
+			}
+			break;
+		case 'remove':
+			if (otlistReg[name]) {
+				delete otlistReg[name];
+				res.write(JSON.stringify({
+					status: 'success',
+					op: 'remove',
+					person: name,
+					table: otlistReg
+				}));
+			}
+			break;
+	}
+	res.end();
+});
+
+var server = http.createServer(app).listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
+});
+
+/**
+ * Socket.IO
+ */
+var io = require('socket.io').listen(server);
+io.sockets.on('connection', function (socket) {
+	onlineCount++;
+	io.sockets.emit('user connected', onlineCount);
+
+	// emit current list on connection
+	socket.emit('init-status',{
+		allowReg: allowReg,
+		currentList: otlistReg,
+		onlineCount: onlineCount
+	});
+
+	// on request-add-person
+	socket.on('request-add-person', function (name) {
+		if(otlistReg[name]){
+			socket.emit('error',{
+				code: '001',
+				msg: 'You already on the list.'
+			});
+		}else{
+			otlistReg[name] = true;
+			io.sockets.emit('add-person',name);
+		}
+	});
+
+	// on request-remove-person
+	socket.on('request-remove-person', function (name) {
+		if(otlistReg[name]){
+			delete otlistReg[name];
+			io.sockets.emit('remove-person',name);
+		}
+	});
+
+	// on disconnect
+	socket.on('disconnect', function () {
+		onlineCount--;
+		io.sockets.emit('user disconnected', onlineCount);
+	});
+});
+
+var checkDeadline = setInterval(function(){
+	var eh = 17, em = 30; // deadline at 5:30 PM
+
+	var h = (new Date).getHours(),
+		m = (new Date).getMinutes();
+	if(h == eh && m > em || h > eh){
+		io.sockets.emit('deadline');
+		allowReg = false;
+		clearInterval(checkDeadline);
+	}
+},900);
---
+++ public/js/main.js
@@ -0,0 +1,307 @@
+(function(){
+	$.extend({
+		replace: function(text,obj){
+			return text.replace(/\{\$(\w+)\}/g,function(x,a){
+				return obj.hasOwnProperty(a) ? obj[a] : x;
+			})
+		},
+		tmpl: function(text,obj){
+			return $($.replace(text,obj));
+		}
+	});
+})();
+
+
+/**
+ * main
+ */
+$(function(){
+
+	var Events = (function(){
+		var eventNames = [
+				'appInit',
+				'addMe',        // request to server
+				'addPerson',
+				'removeMe',     // request to server
+				'removePerson',
+				'inputNameChanged',
+				'error'
+			],
+			events = {};
+		for(var i = eventNames.length; i--;){
+			events[eventNames[i]] = $.Callbacks();
+		}
+		return events;
+	})();
+
+	var Module = (function(){
+		// local name list
+		var cloud = {},
+			socket = io.connect(window.location.host),
+			request = 'http://' + window.location.host + '/otdinner';
+
+		// fire init event on connection
+		socket.on('init-status',function(initStatus){
+			Events['appInit'].fire(initStatus);
+		});
+
+		socket.on('add-person',function(name){
+			Events['addPerson'].fire(name);
+		});
+
+		socket.on('remove-person',function(name){
+			Events['removePerson'].fire(name);
+		});
+
+		socket.on('error',function(errObj){
+			Events['error'].fire(errObj);
+		});
+
+		socket.on('user connected',function(num){
+			View.updateOnlineCount(num);
+		});
+		socket.on('user disconnected',function(num){
+			View.updateOnlineCount(num);
+		});
+
+		socket.on('test',function(data){ console.log(data) });
+
+		return {
+			socket: socket,
+			allowReg: true,
+			/**
+			 * init list from server
+			 */
+			init: function(initStatus){
+			},
+
+			/**
+			 * Add a person to Module(local data).
+			 * @param name
+			 */
+			addPerson: function (name) {
+				// name is the index
+				if(cloud[name]){
+					return;
+				}
+				cloud[name] = true;
+			},
+
+			/**
+			 * Remove a person from Module(local data).
+			 * @param name {String}
+			 */
+			removePerson: function(name){
+				delete cloud[name];
+			},
+
+			/**
+			 * Request adding me to the list table(server side)
+			 * @param name
+			 */
+			addMe: function(name){
+				socket.emit('request-add-person',name);
+			},
+
+			/**
+			 * Request removing me to the list table(server side)
+			 * @param name
+			 */
+			removeMe: function(name){
+				socket.emit('request-remove-person',name);
+			},
+
+			/**
+			 * update localStorage
+			 * @param name
+			 */
+			updateLocalStorage: function(name){
+				localStorage.setItem("user-name", name);
+			},
+
+			getCount: function(){
+				var ret = 0;
+				for(var p in cloud){
+					if(cloud.hasOwnProperty(p)){
+						ret++;
+					}
+				}
+				return ret;
+			}
+		}
+	})();
+	
+	var View = (function(){
+		var $nameInput = $('#name-input'),
+			$signupBtn = $('#signup'),
+			$cloudDiv = $('#list'),
+			$msgArea = $('#msg'),
+			cloudTemplate = '<div class="person alert {$type}" data-name="{$name}">\
+								<button class="close">&times;</button>\
+								<p class="p-name">{$name}</p>\
+							</div>';
+		return {
+			nameInput: $nameInput,
+			signupBtn: $signupBtn,
+			cloudDiv : $cloudDiv,
+
+			/**
+			 * Init the View
+			 */
+			init:function(initStat){
+				// init name from localStorage
+				$nameInput.val(localStorage.getItem('user-name'));
+
+				// check allowReg stat
+				if(!initStat['allowReg']){
+					View.disableReg();
+					$nameInput[0].blur();
+				}
+
+				View.updateCount();
+				View.updateOnlineCount(initStat['onlineCount']);
+			},
+
+			/**
+			 * add person to cloud html element
+			 * @param name
+			 */
+			cloudAddOne: function(name){
+				$.tmpl( cloudTemplate, {
+					"name" : name,
+					"type" : name == $nameInput.val() ? 'alert-success' : 'alert-default'
+				}).appendTo( $cloudDiv );
+				View.updateCount();
+			},
+
+			/**
+			 * remove person from cloud html element
+			 * @param name
+			 */
+			cloudRemoveOne: function(name){
+				var personNode = $cloudDiv.find('[data-name=' + name + ']');
+				personNode.remove();
+				View.updateCount();
+			},
+
+			/**
+			 * pop an alert msg
+			 * @param msg
+			 */
+			alert: function(msg){
+				(function(msg){
+					var $el = $('<div class="alert alert-error">'+msg+'</div>').appendTo($msgArea).slideDown('fast');
+					setTimeout(function(){
+						$el.slideUp('fast',function(){
+							this.parentNode.removeChild(this);
+						})
+					},3000)
+				})(msg);
+			},
+
+			/**
+			 * If user change name input, the highlight item in cloud list will update with it
+			 * @param name
+			 */
+			updateCloudByMyName: function(name){
+				var crowd = $cloudDiv.children();
+				for(var i = crowd.length, one; i--, one = crowd[i];){
+					if(name == 'whosyourdaddy'){
+						one.className = 'person alert alert-success';
+					}else{
+						one.className = 'person alert alert-'
+							+ (one.getAttribute('data-name') == name ? 'success' : 'default')
+					}
+				}
+			},
+
+			/**
+			 * disable input on deadline
+			 */
+			disableReg: function(){
+				if(Module.allowReg){
+					$('#signup').attr('disabled','disabled');
+					$('#name-input').attr('disabled','disabled');
+					View.alert('订餐已截止');
+					Module.allowReg = false;
+				}
+			},
+
+			/**
+			 * enable inputs, maybe we need this?
+			 */
+			enableReg: function(){
+				if(!Module.allowReg){
+					$('#signup').removeAttr('disabled');
+					$('#name-input').removeAttr('disabled');
+				}
+			},
+
+			updateCount: function(){
+				$('#listCount').html(Module.getCount());
+			},
+
+			updateOnlineCount: function(num){
+				$('#onlineCount').html(num);
+			}
+		}
+	})();
+
+	/**
+	 * Init Events and everything
+	 * @type {*}
+	 */
+	var Controller = (function(){
+		Events['appInit'].add(function(initStatus){
+			// init list
+			for (var p in initStatus['currentList']) {
+				if (initStatus['currentList'].hasOwnProperty(p)) {
+					Events['addPerson'].fire(p);
+				}
+			}
+
+			// init view
+			View.init(initStatus);
+		});
+
+		Events['addMe'].add(Module.addMe);
+		Events['addPerson'].add(Module.addPerson,View.cloudAddOne);
+
+		Events['removeMe'].add(Module.removeMe);
+		Events['removePerson'].add(Module.removePerson,View.cloudRemoveOne);
+
+		Events['inputNameChanged'].add(Module.updateLocalStorage, View.updateCloudByMyName);
+
+		Events['error'].add(function (errObj) {
+			switch (errObj['code']) {
+				case '001':
+					View.alert(errObj['msg']);
+					break;
+				default:
+					console.log('Error:', errObj['code'], errObj);
+			}
+		});
+
+		// main button event
+		$('#form').on('submit', function (e) {
+			e.preventDefault();
+			var name = $('#name-input').val();
+			Events['addMe'].fire(name);
+		});
+
+		View.cloudDiv.on('click', function(e){
+			var tar = e.target;
+			if(tar.nodeName == 'BUTTON'){
+				var name = tar.parentNode.getAttribute('data-name');
+				Events['removeMe'].fire(name);
+			}
+		});
+
+		View.nameInput.on('input', function(e){
+			var name = e.target.value;
+			Events['inputNameChanged'].fire(name);
+		});
+	})();
+
+	window.View = View;
+});
