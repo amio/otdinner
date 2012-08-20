146076205a8fa1d575138b1381ac0ecb794425f3
Amio <amio.cn@gmail.com>
2012-08-20 18:31:28 +0800

    Modify for Nodejitsu deploy

--- app.js
+++ app.js
@@ -87,10 +87,10 @@ var io = require('socket.io').listen(server);
 
 // assuming io is the Socket.IO server object
 // for Heroku [https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku]
-io.configure(function () {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
-});
+// io.configure(function () {
+//   io.set("transports", ["xhr-polling"]);
+//   io.set("polling duration", 10);
+// });
 
 
 io.sockets.on('connection', function (socket) {
